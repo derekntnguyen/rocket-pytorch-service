9cebb7aad1037922992b3c39c5c64f24eaab9833
derekntnguyen <79728577+derekntnguyen@users.noreply.github.com>
2023-03-11 21:53:42 -0500

    Creates template rocket service

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
-fn main() {
-    println!("Hello, world!");
+#[macro_use]
+extern crate rocket;
+
+#[get("/")]
+fn root() -> &'static str {
+    "Welcome to the landing page!"
+}
+
+#[get("/hello")]
+fn hello_world() -> &'static str {
+    "hello world"
+}
+
+#[get("/ping")]
+fn ping() -> &'static str {
+    "pong!"
+}
+
+#[launch]
+fn rocket() -> _ {
+    rocket::build()
+        .mount("/", routes![root])
+        .mount("/", routes![hello_world])
+        .mount("/", routes![ping])
 }
